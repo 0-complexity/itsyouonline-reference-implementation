2e1fc9eaf6751ec77487672371f6b4dc946d5825
Dylan Verstraete <dylan_verstraete1@hotmail.com>
2016-07-13 17:44:13 +0200

    Edit

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
  * Created by Dylan on 13/07/16.
  */
 var config = {
-    G8_URL: process.env.G8_URL || 'https://be-scale-1.demo.greenitglobe.com',
+    G8_URL: process.env.G8_URL || 'be-scale-1.demo.greenitglobe.com',
     CLIENT_ID: process.env.CLIENT_ID || "test2",
     callBackUrl: process.env.callBackUrl || "http://localhost:3000/callback",
     client_secret: process.env.client_secret || "V-g38-gf_h52cRHML3WZsG9Pilc09ahP4sIQfYP1mguSrwq50vd5"
