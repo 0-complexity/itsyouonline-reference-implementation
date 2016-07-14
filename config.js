3c3ae381731ad064b50cb47d6610afcaf6e6b95d
Dylan Verstraete <dylan_verstraete1@hotmail.com>
2016-07-14 10:00:49 +0200

    Setting hardcoded callback & secret

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ var config = {
     callBackUrl: process.env.callBackUrl || "http://localhost:3000/callback" || "http://85.255.197.109/callback",
     client_secret: process.env.client_secret || "V-g38-gf_h52cRHML3WZsG9Pilc09ahP4sIQfYP1mguSrwq50vd5" || "lAKqMz_TPwdMzdJ1TrV-v-A4TBtAQlY7-rcW4o1-bY655SuGpiNo"
 }
-config.SCOPE = process.env.SCOPE || "user:name,user:email:user,user:facebook,user:address:main,user:phone:main,user:bankaccount,user:github,user:memberof:" + config.CLIENT_ID
+config.SCOPE = process.env.SCOPE || "user:name,user:email:user,user:facebook,user:address:main,user:phone:main,user:bankaccount,user:github"
 console.log(config);
 module.exports = config;
 
