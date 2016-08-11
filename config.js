0903f4216f2b03e8157911e639d0a1190d7f7f9a
Dylan Verstraete <Dylan_verstraete1@hotmail.com>
2016-08-11 09:13:34 +0200

    G8Url

--- config.js
+++ config.js
@@ -4,8 +4,10 @@
 var config = {
     G8_URL: 'gig.demo.greenitglobe.com',
     CLIENT_ID: process.env.CLIENT_ID || "test2",
-    callBackUrl: process.env.callBackUrl || "http://localhost:3000/callback" || "http://85.255.197.109/callback",
-    client_secret: process.env.client_secret || "V-g38-gf_h52cRHML3WZsG9Pilc09ahP4sIQfYP1mguSrwq50vd5" || "lAKqMz_TPwdMzdJ1TrV-v-A4TBtAQlY7-rcW4o1-bY655SuGpiNo"
+    //"http://localhost:3000/callback"
+    callBackUrl: process.env.callBackUrl || "http://85.255.197.109/callback",
+    //"V-g38-gf_h52cRHML3WZsG9Pilc09ahP4sIQfYP1mguSrwq50vd5"
+    client_secret: process.env.client_secret || "lAKqMz_TPwdMzdJ1TrV-v-A4TBtAQlY7-rcW4o1-bY655SuGpiNo"
 }
 config.SCOPE = process.env.SCOPE || "user:name,user:email:user,user:facebook,user:address:main,user:phone:main,user:bankaccount,user:github,user:memberof:" + config.CLIENT_ID || "user:name,user:email:user,user:facebook,user:address:main,user:phone:main,user:bankaccount,user:github,user:memberof:test2"
 console.log(config);
